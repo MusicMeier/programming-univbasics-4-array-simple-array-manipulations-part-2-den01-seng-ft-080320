710895dc4e9f31763bccd7d1f9053c696d5b19fd
Music Meier <musicm987@gmail.com>
2020-07-29 04:35:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert (array, element)
-array.insert(element[3])
+def using_insert (array, new_element)
+i = 4
+array.insert(element[i])
 end
